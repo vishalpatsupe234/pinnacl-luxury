8253b89eb16791a6e821de11b913015b24edd335
Vishal Patsupe <Everythingispossible234@gmail.com>
2025-11-25 20:16:14 +0530

    chore: add Container, ui-test, tailwind tokens; remove .next from index; ignore hero.mp4

---
+++ tailwind.config.mjs
@@ -0,0 +1,23 @@
+﻿/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'brand-gold': '#C6A667',
+        'brand-black': '#000000',
+        'brand-bg': '#F7F7F7',
+        'brand-charcoal': '#101010',
+        'brand-grey': '#6B6B6B',
+      },
+      fontFamily: {
+        serif: ['Playfair Display', 'serif'],
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
